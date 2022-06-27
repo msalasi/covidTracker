c87298783290bf5a04155f16b5587f688822da8e
msalasi <salasmichael111@gmail.com>
2022-06-27 15:29:37 -0400

    se agregaron iconos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import useCountries from "./hooks/useCountries";
 import UserRow from "./components/UserRow";
 import "./components/Principal.css";
+import tumba from '../src/assets/imgs/lapida-sepulcral.png';
+import virus from '../src/assets/imgs/virus.png';
+import craneo from '../src/assets/imgs/craneo.png';
 
 function App() {
   const {
@@ -19,14 +22,17 @@ function App() {
           <p className="text-white my-2">*Datos con un día de desfase*</p>
           <div className="w-full rounded-lg block sm:block md:flex lg:justify-evenly sm:gap-4 lg:w-5/6 xl:w-5/6 2xl:w-full ">
             <div className="block text-center bg-white mx-auto mb-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 2xl:w-2/6 md:my-2">
+              <img src={virus} alt='virus' className="mx-auto"></img>
               <h1 className="text-black font-sans text-base font-bold">Nuevos confirmados</h1>
               <p className="text-black font-sans text-base font-bold">{GlobalNewConfirmed}</p>
             </div>
             <div className="block text-center bg-white mx-auto my-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 2xl:w-2/6 md:my-2">
+              <img src={tumba} alt='tumba' className="mx-auto"></img>
               <h1 className="text-black font-sans text-base  font-bold">Nuevos muertos</h1>
               <p className="text-black font-sans text-base font-bold">{TotalConmfirmed}</p>
             </div>
             <div className="block text-center bg-white mx-auto mt-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 2xl:w-2/6 md:my-2">
+              <img src={craneo} alt='craneo' className="mx-auto"></img>
               <h1 className="text-black font-sans text-base  font-bold">Total de muertos</h1>
               <p className="text-black font-sans text-base font-bold">{GlobalTotalDeaths}</p>
             </div>
--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import tumba from '../assets/imgs/lapida-sepulcral.png';
+import virus from '../assets/imgs/virus.png';
+
 
 function UserRow({ pais }) {
   return (
     <> 
       <div className="w-full md:w-full lg:w-5/6 xl:w-5/6 2xl:w-full rounded-lg block sm:block sm:justify-evenly sm:gap-4 md:flex">
         <div className="block text-center bg-white mx-auto mb-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 xl:w-2/5 2xl:w-2/6 md:my-2">
+          <img src={virus} alt='virus' className="mx-auto"></img>
           <h1 className="text-black font-sans text-base  font-bold">Total de casos</h1>
           <p className="text-black font-sans text-base font-bold">{pais.TotalConfirmed}</p>
         </div>
         <div className="block text-center bg-white mx-auto my-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 xl:w-2/5 2xl:w-2/6 md:my-2">
+          <img src={virus} alt='virus' className="mx-auto"></img>
           <h1 className="text-black font-sans text-base  font-bold">Casos diarios</h1>
           <p className="text-black font-sans text-base font-bold">{pais.NewConfirmed}</p>
         </div>
         <div className="block text-center bg-white mx-auto mt-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 xl:w-2/5 2xl:w-2/6 md:my-2">
+          <img src={tumba} alt='tumba' className="mx-auto"></img>
           <h1 className="text-black font-sans text-base font-bold">Ultimos fallecidos</h1>
           <p className="text-black font-sans text-base font-bold">{pais.NewDeaths}</p>
         </div>
