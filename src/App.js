20ac82064b0c43ee0b900986cff16055acc1a511
msalasi <salasmichael111@gmail.com>
2022-06-27 14:52:58 -0400

    ultimos detalles

--- src/App.js
+++ src/App.js
@@ -14,24 +14,24 @@ function App() {
   return (
     <>
       <div className="container-virus-info w-full h-screen mx-auto">
-        <div className="backdrop-blur sm:backdrop-blur-none	md:backdrop-blur-none lg:backdrop-blur-none xl:backdrop-blur-none 2xl:backdrop-blur-none sm:mx-auto w-full md:w-9/12 lg:w-8/6 xl:w-3/4 2xl:w-6/12 h-screen items-center flex flex-col justify-center md:w-5/6">
-          <h1 className="text-center text-2xl font-bold font-mono mt-8 p-2 text-white rounded-lg bg-[#b03432] ">Contagios a nivel mundial</h1>
-          <p className="text-white my-4">*Datos con un día de desfase*</p>
+        <div className="backdrop-blur sm:backdrop-blur-none	md:backdrop-blur-none lg:backdrop-blur-none xl:backdrop-blur-none 2xl:backdrop-blur-none sm:mx-auto w-full lg:w-8/6 xl:w-3/4 2xl:w-6/12 h-screen items-center flex flex-col justify-center md:w-5/6">
+          <h1 className="text-center text-2xl font-bold font-mono p-2 text-white rounded-lg bg-[#b03432] ">Contagios a nivel mundial</h1>
+          <p className="text-white my-2">*Datos con un día de desfase*</p>
           <div className="w-full rounded-lg block sm:block md:flex lg:justify-evenly sm:gap-4 lg:w-5/6 xl:w-5/6 2xl:w-full ">
-            <div className="block text-center bg-white mx-auto my-4 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 2xl:w-2/6 shadow-inner">
-              <h1 className="text-black font-mono text-xl  font-bold">Nuevos confirmados</h1>
-              <p className="text-black font-mono text-xl font-bold">{GlobalNewConfirmed}</p>
+            <div className="block text-center bg-white mx-auto mb-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 2xl:w-2/6 shadow-inner">
+              <h1 className="text-black font-mono text-base font-bold">Nuevos confirmados</h1>
+              <p className="text-black font-mono text-base font-bold">{GlobalNewConfirmed}</p>
             </div>
-            <div className="block text-center bg-white mx-auto my-4 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 2xl:w-2/6">
-              <h1 className="text-black font-mono text-xl  font-bold">Nuevos muertos</h1>
-              <p className="text-black font-mono text-xl font-bold">{TotalConmfirmed}</p>
+            <div className="block text-center bg-white mx-auto my-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 2xl:w-2/6">
+              <h1 className="text-black font-mono text-base  font-bold">Nuevos muertos</h1>
+              <p className="text-black font-mono text-base font-bold">{TotalConmfirmed}</p>
             </div>
-            <div className="block text-center bg-white mx-auto my-4 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 2xl:w-2/6">
-              <h1 className="text-black font-mono text-xl  font-bold">Total de muertos</h1>
-              <p className="text-black font-mono text-xl font-bold">{GlobalTotalDeaths}</p>
+            <div className="block text-center bg-white mx-auto mt-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 2xl:w-2/6">
+              <h1 className="text-black font-mono text-base  font-bold">Total de muertos</h1>
+              <p className="text-black font-mono text-base font-bold">{GlobalTotalDeaths}</p>
             </div>
           </div>
-          <h1 className="text-center text-2xl font-bold font-mono my-4 p-2 text-white rounded-lg bg-[#b03432]">Chile</h1>
+          <h1 className="text-center text-2xl font-bold font-mono my-2 p-2 text-white rounded-lg bg-[#b03432]">Chile</h1>
 
           {countryInfo
               .filter((pais) => pais.Country === "Chile")
--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -4,17 +4,17 @@ function UserRow({ pais }) {
   return (
     <> 
       <div className="w-full md:w-full lg:w-5/6 xl:w-5/6 2xl:w-full rounded-lg block sm:block sm:justify-evenly sm:gap-4 md:flex">
-        <div className="block text-center bg-white mx-auto my-4 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 xl:w-2/5 2xl:w-2/6">
-          <h1 className="text-black font-mono text-xl  font-bold">Total de casos</h1>
-          <p className="text-black font-mono text-xl font-bold">{pais.TotalConfirmed}</p>
+        <div className="block text-center bg-white mx-auto mb-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 xl:w-2/5 2xl:w-2/6">
+          <h1 className="text-black font-mono text-base  font-bold">Total de casos</h1>
+          <p className="text-black font-mono text-base font-bold">{pais.TotalConfirmed}</p>
         </div>
-        <div className="block text-center bg-white mx-auto my-4 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 xl:w-2/5 2xl:w-2/6">
-          <h1 className="text-black font-mono text-xl  font-bold">Casos diarios</h1>
-          <p className="text-black font-mono text-xl font-bold">{pais.NewConfirmed}</p>
+        <div className="block text-center bg-white mx-auto my-2 p-4 rounded-lg w-3/5 sm:w-1/3 md:w-2/5 lg:w-1/3 xl:w-2/5 2xl:w-2/6">
+          <h1 className="text-black font-mono text-base  font-bold">Casos diarios</h1>
+          <p className="text-black font-mono text-base font-bold">{pais.NewConfirmed}</p>
         </div>
-        <div className="block text-center bg-white mx-auto my-4 p-4 rounded-lg w-3/5 sm:w-2/5 md:w-2/5 lg:w-1/3 xl:w-2/5 2xl:w-2/6">
-          <h1 className="text-black font-mono text-xl font-bold">Ultimos fallecidos</h1>
-          <p className="text-black font-mono text-xl font-bold">{pais.NewDeaths}</p>
+        <div className="block text-center bg-white mx-auto mt-2 p-4 rounded-lg w-3/5 sm:w-2/5 md:w-2/5 lg:w-1/3 xl:w-2/5 2xl:w-2/6">
+          <h1 className="text-black font-mono text-base font-bold">Ultimos fallecidos</h1>
+          <p className="text-black font-mono text-base font-bold">{pais.NewDeaths}</p>
         </div>
       </div>
     </>
